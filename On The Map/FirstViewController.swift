d51bee402125b7b9a5f16db3ff35a76557ba86e0
rdsmith675 <Rdsmith675@icloud.com>
2016-03-29 12:42:42 -0400

    Initial Commit

---
+++ On The Map/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  On The Map
+//
+//  Created by Richard smith on 3/29/16.
+//  Copyright © 2016 Richard smith. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
